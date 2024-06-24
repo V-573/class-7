680e9cb0a93a99f4d80bd557947f58fc00492cfa
vaj573 <jhonedw573@gmail.com>
2024-06-24 18:27:34 -0500

    agregue estilos y acomode en localstorage las transacciones de deposit y transaction para mostrar las cantidades hechas

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,100 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import Timer from "./Timer";
-
+import { ContainerPpal, InputPpal } from "./assets/StylePpal";
 
 function App() {
+  const [withdraw, setWithdraw] = useState(0)
+  const [deposit, setDeposit] = useState(0)
   const [count, setCount] = useState(0);
   const [show, setShow] = useState(true);
-  const [transactions, setTransactions] = useState([
-  
-  ]);
-
-    // { id: 1, amount: 20, date: new Date(), type: "withdraw" },
-    // { id: 2, amount: 154, date: new Date(), type: "withdraw" },
-    // { id: 3, amount: 250, date: new Date(), type: "deposit" },
-    // { id: 4, amount: 2000, date: new Date(), type: "withdraw" },
-    // { id: 5, amount: 650, date: new Date(), type: "deposit" },
-
-   const [showTimer, setShowTimer] = useState(true); // ESTADO PARA MANEJAR EL TIMER
-
+  const [transactions, setTransactions] = useState([]);
+  const [showTimer, setShowTimer] = useState(true); // ESTADO PARA MANEJAR EL TIMER
   const STORAGE_DATA = " DATA_TRANSACTION";
-  
+  const STORAGE_DEPOSIT = "STORAGED_DEPOSIT";
+  const STORAGE_WITHDRAW = "STORAGED_WITHDRAW";
 
   // Leer las transacciones del localStorage cuando se carga el componente
   useEffect(() => {
     const storedTransactions = localStorage.getItem(STORAGE_DATA);
     if (storedTransactions) {
-      const parsedTransactions = JSON.parse(storedTransactions).map(transaction => ({
-        ...transaction,
-        date: new Date(transaction.date)
-      }));
+      const parsedTransactions = JSON.parse(storedTransactions).map(
+        (transaction) => ({
+          ...transaction,
+          date: new Date(transaction.date),
+        })
+      );
+
       setTransactions(parsedTransactions);
     }
+
+      const storedDeposit = localStorage.getItem(STORAGE_DEPOSIT);
+      const storedWithdraw = localStorage.getItem(STORAGE_WITHDRAW);
+
+      if (storedDeposit) {
+        setDeposit(JSON.parse(storedDeposit))
+      }
+      if (storedWithdraw) {
+  setWithdraw(JSON.parse(storedWithdraw))
+}
+
   }, []);
 
 
 
 
+
   const handleDeleteTransaction = (transaction) => {
-
-    setTransactions ((p) =>{
-    const updatedTransactions = p.filter((t) => t.id != transaction.id);
-    localStorage.setItem(STORAGE_DATA, JSON.stringify(updatedTransactions));
+    setTransactions((p) => {
+      const updatedTransactions = p.filter((t) => t.id != transaction.id);
+      localStorage.setItem(STORAGE_DATA, JSON.stringify(updatedTransactions));
       return updatedTransactions;
-  });
-
+    });
   };
 
   const handleTransaction = (e) => {
     e.preventDefault();
     const { amount, type } = e.target.elements;
-       
+
+
+
+    if (type.value === "withdraw") {
+      setWithdraw((w) => {
+        const updatedWithdraw = w + 1;
+        
+        localStorage.setItem(STORAGE_WITHDRAW, JSON.stringify(updatedWithdraw));
+        return updatedWithdraw;
+      });
+    } else 
+    {
+      setDeposit((d) => {
+        const updatedDeposit = d + 1;
+        localStorage.setItem(STORAGE_DEPOSIT, JSON.stringify(updatedDeposit))
+        return updatedDeposit;
+      });
+     
+
+      }
+
     const newTransaction = {
       id: uuidv4(),
       type: type.value,
       amount: amount.value,
       date: new Date(),
     };
-    setTransactions(p => {
+    setTransactions((p) => {
       const updatedTransactions = [...p, newTransaction];
 
       localStorage.setItem(STORAGE_DATA, JSON.stringify(updatedTransactions));
       return updatedTransactions;
     });
+
+    
+    
+
+
+
   };
 
   const handleCount = () => {
@@ -73,52 +105,41 @@ function App() {
     setShow((s) => !s);
   };
 
-
   //MANEJADOR DELEVENTO PARA MSTRAR EL TIMER
   const toggleTimer = () => {
     setShowTimer((t) => !t);
   };
 
-
-
   return (
     <>
-      <div
-        style={{
-          height: "300px",
-          display: "flex",
-          flexDirection: "left",
-          justifyContent: "left",
-          alignItems: "flex-start",
-        }}
-      >
+      <ContainerPpal>
         <h1> hello </h1>
         <button onClick={handleCount}>add one: {count}</button>
         {show ? <h3>This is a message</h3> : <h3>no esta el message</h3>}
         <button onClick={handleShow}>show message</button>
-      </div>
+      </ContainerPpal>
 
       <div>
-        <h2>EJEMPLO 2 CON TRANSACTIONS {showTimer &&  <Timer/> }</h2>
-        <button onClick={toggleTimer}>{showTimer ? 'HIDE TIME' : 'SHOW TIMER'}</button>
+        <h2>EJEMPLO 2 CON TRANSACTIONS {showTimer && <Timer />}</h2>
+        <button onClick={toggleTimer}>
+          {showTimer ? "HIDE TIME" : "SHOW TIMER"}
+        </button>
 
-        <form
-        style={{border:"1px solid black", padding:"22px", display:"flex", flexDirection:"column", gap:"15",marginBottom:"30px",}}
-         onSubmit={ handleTransaction }
-        
-        >
+        <div style={{display:"flex", flexDirection:"column"}}>
+          <span>cantidad de transacciones con deposito: { deposit}</span>
+          <span>cantidad de transacciones con retiro: { withdraw}</span>
+        </div>
 
-         
-
-          <h4 style={{margin:0}}>Create a new transaction</h4>
-          <input type="text" placeholder="type the amount" name="amount"/>
+        <InputPpal onSubmit={handleTransaction}>
+          <h4>Create a new transaction</h4>
+          <input type="text" placeholder="type the amount" name="amount" />
           <select name="type">
             <option disabled>Select the type</option>
             <option value="deposit">Deposit</option>
             <option value="withdraw">Withdraw</option>
           </select>
           <button type="submit">Create</button>
-        </form>
+        </InputPpal>
         <table>
           <thead>
             <tr>
@@ -130,14 +151,17 @@ function App() {
           </thead>
 
           <tbody>
-             {transactions.map((transaction) => (
+            {transactions.map((transaction) => (
               <tr key={transaction.id}>
-                
-                 <td>{transaction.type}</td>
+                <td>{transaction.type}</td>
                 <td>{transaction.date.toUTCString()}</td>
-                 <td>{transaction.amount}</td>
-                 <td><button onClick={() => handleDeleteTransaction(transaction)} >DELETE</button></td>
-                 {/*  usar la funcion flecha garantiza garantiza que la funcion que estoy llamando se ejecute cuando se haga click. Hay que tener encuenta que aca estoy en el renderizado de la app */}
+                <td>{transaction.amount}</td>
+                <td>
+                  <button onClick={() => handleDeleteTransaction(transaction)}>
+                    DELETE
+                  </button>
+                </td>
+                {/*  usar la funcion flecha garantiza garantiza que la funcion que estoy llamando se ejecute cuando se haga click. Hay que tener encuenta que aca estoy en el renderizado de la app */}
               </tr>
             ))}
           </tbody>
---
+++ src/assets/StylePpal.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ContainerPpal = styled.div`
+height:300px;
+display: flex;
+flex-direction: center;
+justify-content: left;
+align-items: center; 
+
+
+
+`;
+
+export const InputPpal = styled.form`
+border: 1px solid black;
+padding: 22px;
+display: flex;
+flex-direction: column;
+gap: 15px;
+margin-bottom:30px;
+
+
+
+`;
